64b3ddb2cd603243c5ffed67bf68515e14c5e36a
Marcel <malbrecht0792@gmail.com>
2017-04-06 17:22:43 -0500

    Completed all 3 user/post/comment models and their associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
 	validates :title, presence: true, length: { minimum: 6 }
 	validates :body, presence: true, length: { maximum: 40000 }
+	validates :user_id, presence: true
 	belongs_to :user
+	has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
 	validates :username, presence: true, length: { minimum: 6 }, uniqueness: true
 	has_many :posts
+	has_many :comments
 end
